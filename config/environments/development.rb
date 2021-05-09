3ecb30039bbae760acf0e6ca4ed1ccdc66e8b7c6
Ubuntu <ubuntu@ip-172-31-32-67.ec2.internal>
2021-05-09 16:37:04 +0000

    completing initial assignment for alpha-blog

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "a5f9c2ec9dd040e293c01bebbc41addb.vfs.cloud9.us-east-1.amazonaws.com"
 end
